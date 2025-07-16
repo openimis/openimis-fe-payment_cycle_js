5e4f9df53d32c24bf2ceecbe99f237bdbebf8611
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-07-16 23:22:31 +0530

    migrated from the cra to vite

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.jsx'),
+      name: 'OpenImisFePaymentCycle',
+      fileName: (format) => `index.${format}.js`,
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: [
+        /^@babel.*/,
+        /^@date-io\/.*/,
+        /^@material-ui\/.*/,
+        /^@openimis.*/,
+        'classnames',
+        'clsx',
+        'history',
+        /^lodash.*/,
+        'moment',
+        'prop-types',
+        /^react.*/,
+        /^redux.*/,
+      ],
+    },
+    outDir: 'dist',
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+});
