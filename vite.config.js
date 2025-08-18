c447337224a9b81af25c3b82d0036f0ecd49fc44
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-08-19 00:57:07 +0530

    fix(build): correct file extension

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/index.jsx'),
       name: 'OpenImisFePaymentCycle',
-      fileName: (format) => `index.${format}.js`,
+      fileName: (format) => `index.${format === 'es' ? 'es' : 'cjs'}.js`,
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
